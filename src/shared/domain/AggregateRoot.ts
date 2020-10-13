d72d7d6f486ee0e1d307cc2208ab9d7c3db2bb3d
Petteri Suominen <petteri.suominen@gmail.com>
2020-10-13 21:25:01 +0300

    base models

--- src/shared/domain/AggregateRoot.ts
+++ src/shared/domain/AggregateRoot.ts
@@ -2,7 +2,7 @@
 import { Entity } from "./Entity";
 import { UniqueEntityID } from "./UniqueEntityID";
 
-// This class will implement domain events
+// This class will implement domain events later
 export abstract class AggregateRoot<T> extends Entity<T> {
   get id (): UniqueEntityID {
     return this._id;
